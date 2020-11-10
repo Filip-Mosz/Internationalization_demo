44b22bf06486e04d47449280118a4cf593dc0eb9
Filip M <filip.moszczynski@onet.pl>
2020-11-10 20:28:04 +0100

    international.html added

--- src/main/java/com/example/config/MvcConfig.java
+++ src/main/java/com/example/config/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.Internationalization_demo;
+package com.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
