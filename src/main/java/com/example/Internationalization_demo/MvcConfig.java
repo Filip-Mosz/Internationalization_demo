f193566b001e1f7c9a56d44603aceceb3a446b98
Filip M <filip.moszczynski@onet.pl>
2020-11-10 19:54:22 +0100

    LocaleChangeInterceptor

--- src/main/java/com/example/Internationalization_demo/MvcConfig.java
+++ src/main/java/com/example/Internationalization_demo/MvcConfig.java
@@ -1,13 +1,19 @@
 package com.example.Internationalization_demo;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+
 
 
     @Bean
@@ -16,4 +22,19 @@ public class MvcConfig implements WebMvcConfigurer {
         slr.setDefaultLocale(Locale.ENGLISH);
         return slr;
     }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() { //this gives possibility to change default localization
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) { //adds localeChangeInterceptor() to interceptor registry
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+    //when class haven't @Configuration decorator this method was marked as creating problem
+
+
 }
